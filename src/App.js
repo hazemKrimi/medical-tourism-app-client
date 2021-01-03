a141da5667493258df87d5abbd1b60a4fec020e6
Hazem Krimi <krimihazem1@gmail.com>
2021-01-03 23:30:11 +0100

    Add app routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Container } from '@chakra-ui/react';
+import { Switch } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Landing from './pages/Landing';
+import Settings from './pages/Settings';
+import ProtectedRoute from './components/ProtectedRoute';
+import AuthRoute from './components/AuthRoute';
+
 const App = () => {
-	return <></>;
+	return (
+		<Container maxWidth='85%'>
+			<Nav />
+			<Switch>
+				<AuthRoute exact path='/'>
+					<Landing />
+				</AuthRoute>
+				<ProtectedRoute exact path='/home'>
+					<Home />
+				</ProtectedRoute>
+				<ProtectedRoute exact path='/settings'>
+					<Settings />
+				</ProtectedRoute>
+			</Switch>
+		</Container>
+	);
 };
 
 export default App;
